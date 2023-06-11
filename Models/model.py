111852a05b89964ffb766f2cc7e702d37bcc9031
Admin <2103shivamk@gmail.com>
2023-06-11 23:08:42 +0530

    Some Refactoring of Code

--- Models/model.py
+++ Models/model.py
@@ -1,13 +1,8 @@
 # Imports
 from pytube import YouTube
-import cv2
-from PIL import Image
 import clip
 import torch
-import math
 from pytube import YouTube
-import cv2
-import yt_dlp as youtube_dl
 import pvleopard
 import os
 import shutil
@@ -21,144 +16,74 @@ from pydub import AudioSegment
 from pydub.utils import make_chunks
 from sentence_transformers import SentenceTransformer
 from numpy.linalg import norm
-# loading Model and constants
-
+import video_processing as vp
+import clip
+##Preload Model and Preprocess
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load("ViT-B/32", device=device)
-N = 60
+
+#51 seconds
+# Prediction
+def search_video(search_query, link, type="yt", display_results_count=3):
+    #Old Code which required downloading the video
+    # video_link=video_download(link,type)
+    # fps,video_frames=video_frames_f(video_link)
+
+    #Extract Frames without downloading
+    fps, video_frames = vp.video_preprocess(link, type)
+    #Adds NLP encoding features to the frames
+    video_features = vp.frame_classifier(video_frames, device, model, preprocess)
+    #Adds NLP encoding to the search query
+    with torch.no_grad():
+        text_features = model.encode_text(clip.tokenize(search_query).to(device))
+        text_features /= text_features.norm(dim=-1, keepdim=True)
+    #Finds similarity between the search query and the frames
+    similarities = 100.0 * video_features @ text_features.T
+    values, best_photo_idx = similarities.topk(display_results_count, dim=0)
+    # Display the top 3 frames
+    time_stamps = []
+    for frame_id in best_photo_idx:
+        seconds = round(frame_id.cpu().numpy()[0] * vp.N / fps)
+        time_stamps.append(seconds)
+    return time_stamps
 
 
 # Audio Download Function
-def video_download(link, type):
+def videoForAudio_download(link, type):
     print("Video Downloading")
-    if type == 'yt':
+    if type == "yt":
         yt = YouTube(link)
         video = yt.streams.filter(only_audio=True).first()
         out_file = video.download(output_path=".")
         # save the file
         base, ext = os.path.splitext(out_file)
-        new_file = base + '.mp3'
+        new_file = base + ".mp3"
         os.rename(out_file, new_file)
         return new_file
     elif type == "static":
-        urllib.request.urlretrieve(link, str(link.split("token=")[-1])+'.mp4')
-        clip = mp.VideoFileClip((link.split("token=")[-1])+'.mp4')
+        urllib.request.urlretrieve(link, str(link.split("token=")[-1]) + ".mp4")
+        clip = mp.VideoFileClip((link.split("token=")[-1]) + ".mp4")
         clip.audio.write_audiofile(r"converted.wav")
         return "converted.wav"
 
-# #Video Preprocess
-# def video_frames_f(link):
-#     print("Video Preprocesing")
-#     video_frames = []  #Frames List
-#     capture = cv2.VideoCapture(link)
-#     fps = capture.get(cv2.CAP_PROP_FPS)
-#     current_frame = 0
-#     while capture.isOpened():
-#         ret, frame = capture.read()
-#         if ret == True:
-#             video_frames.append(Image.fromarray(frame[:, :, ::-1]))
-#         else:
-#             break
-#         current_frame += N #Skip frames
-#         capture.set(cv2.CAP_PROP_POS_FRAMES, current_frame)
-#     # Print some statistics
-#     print(f"Frames extracted: {len(video_frames)}")
-#     return fps,video_frames
-
-# Capturing the video frames without downloading
-
-
-def video_preprocess(link, type):
-    print("Extracting Frames")
-    video_frames = []  # Frames List
-    # If-Else to get the vid_capture object from the link and type
-    if type == 'yt':
-        ydl_opts = {}
-        ydl = youtube_dl.YoutubeDL(ydl_opts)
-        info_dict = ydl.extract_info(link, download=False)
-        formats = info_dict.get('formats', None)
-        for f in formats:
-            if f.get('format_note', None) == '144p':
-                url = f.get('url', None)
-                vid_capture = cv2.VideoCapture(url)
-    elif type == 'static':
-        vid_capture = cv2.VideoCapture(link)
-
-    # Start Capturing
-    current_frame = 0
-    fps = vid_capture.get(cv2.CAP_PROP_FPS)
-    while True:
-        ret, frame = vid_capture.read()
-        if not ret:
-            break
-        video_frames.append(Image.fromarray(frame[:, :, ::-1]))
-        current_frame += N  # Skip frames
-        vid_capture.set(cv2.CAP_PROP_POS_FRAMES, current_frame)
-    vid_capture.release()
-
-    print(f"Frames extracted: {len(video_frames)}")
-    return fps, video_frames
-
-# Adding context to each frame
-
-
-def frame_classifier(video_frames):
-    batch_size = 256
-    batches = math.ceil(len(video_frames) / batch_size)
-    video_features = torch.empty(
-        [0, 512], dtype=torch.float16).to(device)  # Encoded Frames
-    # Frames Processing
-    for i in range(batches):
-        print(f"Processing batch {i+1}/{batches}")
-        batch_frames = video_frames[i*batch_size: (i+1)*batch_size]
-        batch_preprocessed = torch.stack(
-            [preprocess(frame) for frame in batch_frames]).to(device)
-        with torch.no_grad():
-            batch_features = model.encode_image(batch_preprocessed)
-            batch_features /= batch_features.norm(dim=-1, keepdim=True)
-        video_features = torch.cat((video_features, batch_features))
-    # Print some stats
-    print(f"Features Shape: {video_features.shape}")
-    return video_features
-
-# Prediction
-
-
-def search_video(search_query, link, type='yt', display_results_count=3):
-    time_stamps = []
-    # video_link=video_download(link,type)
-    # fps,video_frames=video_frames_f(video_link)
-    fps, video_frames = video_preprocess(link, type)
-    video_features = frame_classifier(video_frames)
-    with torch.no_grad():
-        text_features = model.encode_text(
-            clip.tokenize(search_query).to(device))
-        text_features /= text_features.norm(dim=-1, keepdim=True)
-    similarities = (100.0 * video_features @ text_features.T)
-    values, best_photo_idx = similarities.topk(display_results_count, dim=0)
-    # Display the top 3 frames
-    for frame_id in best_photo_idx:
-        seconds = round(frame_id.cpu().numpy()[0] * N / fps)
-        time_stamps.append(seconds)
-    return time_stamps
 
 # Audio Hot Words
 
 
 def transcript(file):
     leopard = pvleopard.create(
-        access_key='clzWvIxwJZXvEIc4rhS9oBA3yoJ8GzcTqZfvn6KuqL/M7FPcB2HJWQ==')
+        access_key="clzWvIxwJZXvEIc4rhS9oBA3yoJ8GzcTqZfvn6KuqL/M7FPcB2HJWQ=="
+    )
     file = os.path.abspath(file)
     tsc, words = leopard.process_file(file)
     return tsc
 
 
 def hot_words(link, type):
-    file = video_download(link, type)
+    file = videoForAudio_download(link, type)
     # file=os.path.abspath("converted.wav")
     print("Transcribing")
-    vectorizer = TfidfVectorizer(
-        max_features=10, stop_words=stopwords.words('english'))
+    vectorizer = TfidfVectorizer(max_features=10, stop_words=stopwords.words("english"))
     corpus = [transcript(file)]
     X = vectorizer.fit_transform(corpus).todense()
     X1 = np.array(X).flatten()
@@ -170,10 +95,10 @@ def hot_words(link, type):
 
 def chunks(file):
     try:
-        os.mkdir('chunks')
+        os.mkdir("chunks")
     except:
-        shutil.rmtree('chunks')
-        os.mkdir('chunks')
+        shutil.rmtree("chunks")
+        os.mkdir("chunks")
     myaudio = AudioSegment.from_file(file)
     chunk_length_ms = 100000  # pydub calculates in millisec
     chunks = make_chunks(myaudio, chunk_length_ms)  # Make chunks of 100 sec
@@ -193,7 +118,7 @@ def query(file, query):
     for chunk in chunk_list:
         sentences.append(transcript(chunk))
 
-    model = SentenceTransformer('all-MiniLM-L6-v2')
+    model = SentenceTransformer("all-MiniLM-L6-v2")
     sentence_embeddings = model.encode(sentences)
     sentence_embeddings.shape
     query = [query]
@@ -201,7 +126,8 @@ def query(file, query):
     query_embedding = model.encode(query).reshape(1, -1)
     similarities = []
     for i in range(len(sentences)):
-        cosine = np.dot(sentence_embeddings[i], query_embedding[0])/(
-            norm(sentence_embeddings[i])*norm(query_embedding[0]))
+        cosine = np.dot(sentence_embeddings[i], query_embedding[0]) / (
+            norm(sentence_embeddings[i]) * norm(query_embedding[0])
+        )
         similarities.append(cosine)
-    return [int((x)*100) for x in np.argsort(similarities)[-1:-4:-1]]
+    return [int((x) * 100) for x in np.argsort(similarities)[-1:-4:-1]]
---
+++ Models/video_processing.py
@@ -0,0 +1,85 @@
+# Imports
+
+import cv2
+from PIL import Image
+import torch
+import math
+import cv2
+import yt_dlp as youtube_dl
+from sklearn.feature_extraction.text import TfidfVectorizer
+N = 60
+# loading Model and constants
+# #Video Preprocess
+# def video_frames_f(link):
+#     print("Video Preprocesing")
+#     video_frames = []  #Frames List
+#     capture = cv2.VideoCapture(link)
+#     fps = capture.get(cv2.CAP_PROP_FPS)
+#     current_frame = 0
+#     while capture.isOpened():
+#         ret, frame = capture.read()
+#         if ret == True:
+#             video_frames.append(Image.fromarray(frame[:, :, ::-1]))
+#         else:
+#             break
+#         current_frame += N #Skip frames
+#         capture.set(cv2.CAP_PROP_POS_FRAMES, current_frame)
+#     # Print some statistics
+#     print(f"Frames extracted: {len(video_frames)}")
+#     return fps,video_frames
+
+# Capturing the video frames without downloading
+
+
+def video_preprocess(link, type):
+    print("Extracting Frames")
+    video_frames = []  # Frames List
+    # If-Else to get the vid_capture object from the link and type
+    if type == 'yt':
+        ydl_opts = {}
+        ydl = youtube_dl.YoutubeDL(ydl_opts)
+        info_dict = ydl.extract_info(link, download=False)
+        formats = info_dict.get('formats', None)
+        for f in formats:
+            if f.get('format_note', None) == '144p':
+                url = f.get('url', None)
+                vid_capture = cv2.VideoCapture(url)
+    elif type == 'static':
+        vid_capture = cv2.VideoCapture(link)
+
+    # Start Capturing
+    current_frame = 0
+    fps = vid_capture.get(cv2.CAP_PROP_FPS)
+    while True:
+        ret, frame = vid_capture.read()
+        if not ret:
+            break
+        video_frames.append(Image.fromarray(frame[:, :, ::-1]))
+        current_frame += N  # Skip frames
+        vid_capture.set(cv2.CAP_PROP_POS_FRAMES, current_frame)
+    vid_capture.release()
+
+    print(f"Frames extracted: {len(video_frames)}")
+    return fps, video_frames
+
+# Adding context to each frame
+
+
+def frame_classifier(video_frames,device,model,preprocess):
+    batch_size = 256
+    batches = math.ceil(len(video_frames) / batch_size)
+    video_features = torch.empty(
+        [0, 512], dtype=torch.float16).to(device)  # Encoded Frames
+    # Frames Processing
+    for i in range(batches):
+        print(f"Processing batch {i+1}/{batches}")
+        batch_frames = video_frames[i*batch_size: (i+1)*batch_size]
+        batch_preprocessed = torch.stack(
+            [preprocess(frame) for frame in batch_frames]).to(device)
+        with torch.no_grad():
+            batch_features = model.encode_image(batch_preprocessed)
+            batch_features /= batch_features.norm(dim=-1, keepdim=True)
+        video_features = torch.cat((video_features, batch_features))
+    # Print some stats
+    print(f"Features Shape: {video_features.shape}")
+    return video_features
